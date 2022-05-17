a9fc13e6f14bfe74e944b9d95b77317b24aebf01
chazh <chazh@LAPTOP-BR9FJPE6>
2022-05-17 13:06:03 -0600

    Add project files.

---
+++ Lamdas/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+var myNumbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+var numbersOver5 = myNumbers.Where(n => n > 5);
+*/
+
+var table = new List<int>{ 1, 2, 3, 4, 5, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+var newTable = table.Where(n => n < 5);
+string combinedString = String.Join(",", newTable);
+
+Console.Write(combinedString);
+Console.Read();
